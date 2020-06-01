8d74cc53e55e301ada5cfee7552c471faddc2dca
Sviatoslav <sv_dubov@ukr.net>
2020-06-01 16:00:26 +0300

    Add tables with migrations

---
+++ database/migrations/2020_06_01_123642_create_articles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->date('date')->nullable();
+            $table->integer('category_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->integer('status')->default(0);
+            $table->integer('views')->default(0);
+            $table->integer('is_featured')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
