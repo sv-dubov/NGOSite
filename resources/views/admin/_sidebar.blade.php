8322a0581054b0f49b707d42af71b41c62dbdecf
Sviatoslav <sv_dubov@ukr.net>
2020-06-02 12:48:24 +0300

    Use webpack, begin admin dashboard

---
+++ resources/views/admin/_sidebar.blade.php
@@ -0,0 +1,21 @@
+<ul class="sidebar-menu">
+    <li class="header">MAIN NAVIGATION</li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Admin-panel</span>
+        </a>
+    </li>
+    <li><a href="#"><i class="fa fa-sticky-note-o"></i> <span>Posts</span></a></li>
+    <li><a href="#"><i class="fa fa-list-ul"></i> <span>Categories</span></a></li>
+    <li><a href="#"><i class="fa fa-tags"></i> <span>Tags</span></a></li>
+    <li>
+        <a href="/admin/comments">
+            <i class="fa fa-commenting"></i> <span>Comments</span>
+            <span class="pull-right-container">
+                <small class="label pull-right bg-red">5</small>
+            </span>
+        </a>
+    </li>
+    <li><a href="#"><i class="fa fa-users"></i> <span>Users</span></a></li>
+    <li><a href="#"><i class="fa fa-user-plus"></i> <span>Subscribers</span></a></li>
+</ul>
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.layout')
+
+@section('content')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Hi! This is admin panel
+            <small>good words..</small>
+        </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Main page</h3>
+            </div>
+            <div class="box-body">
+                Instruction for using admin panel
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                some text
+            </div>
+            <!-- /.box-footer-->
+        </div>
+        <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
