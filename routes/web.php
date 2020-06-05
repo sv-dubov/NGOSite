bd32ac80b7936313b577164ab828f2be394c7b9b
Sviatoslav <sv_dubov@ukr.net>
2020-06-05 20:41:59 +0300

    Hide routes with middleware

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,21 @@ Route::get('/post/{slug}', 'HomeController@show')->name('post.show');
 Route::get('/tag/{slug}', 'HomeController@tag')->name('tag.show');
 Route::get('/category/{slug}', 'HomeController@category')->name('category.show');
 
-Route::get('/register', 'AuthController@registerForm');
-Route::post('/register', 'AuthController@register');
-Route::get('/login','AuthController@loginForm')->name('login');
-Route::post('/login', 'AuthController@login');
-Route::get('/logout', 'AuthController@logout');
+Route::group(['middleware'	=>	'auth'], function(){
+	Route::get('/logout', 'AuthController@logout');
+});
 
-Route::get('/admin', 'Admin\DashboardController@index');
-Route::resource('/admin/categories', 'Admin\CategoriesController');
+Route::group(['middleware'	=>	'guest'], function(){
+	Route::get('/register', 'AuthController@registerForm');
+	Route::post('/register', 'AuthController@register');
+	Route::get('/login','AuthController@loginForm')->name('login');
+	Route::post('/login', 'AuthController@login');
+});
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware'	=>	'admin'], function () {
     Route::get('/', 'DashboardController@index');
     Route::resource('/categories', 'CategoriesController');
     Route::resource('/tags', 'TagsController');
     Route::resource('/users', 'UsersController');
-    Route::resource('/posts', 'PostsController');
+	Route::resource('/posts', 'PostsController');
 });
