a7266492a7a1202639d6ef90abf6f4d48e13ee12
Sviatoslav <sv_dubov@ukr.net>
2020-08-11 16:18:45 +0300

    Edit login and register views

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -2,42 +2,42 @@
 
 @section('content')
 
-<!--main content start-->
-<div class="main-content">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="leave-comment mr0">
-                    <!--leave comment-->
-                    <h3 class="text-uppercase">Register</h3>
-                    @include('admin.errors')
-                    <br>
-                    <form class="form-horizontal contact-form" role="form" method="post" action="/register">
-                        {{csrf_field()}}
-                        <div class="form-group">
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Name" value="{{old('name')}}">
+    <!--main content start-->
+    <div class="main-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="leave-comment mr0">
+                        <!--leave comment-->
+                        <h3>Register</h3>
+                        @include('admin.errors')
+                        <br>
+                        <form class="form-horizontal contact-form" role="form" method="post" action="/register">
+                            {{csrf_field()}}
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Name" value="{{old('name')}}">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="{{old('email')}}">
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" id="email" name="email" placeholder="E-mail" value="{{old('email')}}">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-12">
-                                <input type="password" class="form-control" id="password" name="password" placeholder="password">
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                </div>
                             </div>
-                        </div>
-                        <button type="submit" class="btn send-btn">Register</button>
-                    </form>
+                            <button type="submit" class="btn btn-sm btn-success">Register</button>
+                        </form>
+                    </div>
+                    </br>
+                    <!--end leave comment-->
                 </div>
-                <!--end leave comment-->
             </div>
-            @include('pages._sidebar')
         </div>
     </div>
-</div>
-<!-- end main content-->
+    <!-- end main content-->
 
 @endsection
