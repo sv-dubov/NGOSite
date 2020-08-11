8fe83e083208052871eccfffce18f7340fe8a86e
Sviatoslav <sv_dubov@ukr.net>
2020-08-11 16:14:45 +0300

    Edit layout and index pages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,188 @@
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>NGO Website</title>
+    <link rel="shortcut icon" href="/images/fav.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="/images/fav.jpg">
+    <!-- common css -->
+    <link rel="stylesheet" href="/css/front2.css">
+    @yield('assets')
+</head>
+
+<body>
+<header class="continer-fluid ">
+    <div class="header-top">
+        <div class="container">
+            <div class="row col-det">
+                <div class="col-lg-6 d-none d-lg-block">
+                    <ul class="ulleft">
+                        <li>
+                            <i class="far fa-envelope"></i>
+                            sv_dubov@ukr.net
+                            <span>|</span></li>
+                        <li>
+                            <i class="fas fa-phone-volume"></i>
+                            +050 101 76 89</li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 folouws">
+                    <ul class="ulright">
+                        <li> <small>Folow Us </small>:</li>
+                        <li>
+                            <a href="https://www.facebook.com/sviatoslav.dubov"><i class="fab fa-facebook-square"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/dubov_rv"><i class="fab fa-twitter-square"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/sbf_rivne/"><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fab fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 d-none d-md-block col-md-6 btn-bhed">
+                    @if(Auth::check())
+                        <button class="btn btn-sm btn-success"><a href="/profile">My Profile</a></button>
+                        <button class="btn btn-sm btn-default"><a href="/logout">Logout</a></button>
+                    @else
+                        <button class="btn btn-sm btn-success"><a href="/login">Login</a></button>
+                        <button class="btn btn-sm btn-default"><a href="/register">Register</a></button>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="menu-jk" class="header-bottom">
+        <div class="container">
+            <div class="row nav-row">
+                <div class="col-lg-3 col-md-12 logo">
+                    <a href="/">
+                        <img src="/images/logo.jpg" alt="">
+                        <a data-toggle="collapse" data-target="#menu" href="#menu"><i class="fas d-block d-lg-none small-menu fa-bars"></i></a>
+                    </a>
+                </div>
+                <div id="menu" class="col-lg-9 col-md-12 d-none d-lg-block nav-col">
+                    <ul class="navbad">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/posts">News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/articles">Articles</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/videos">Videos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/gallery">Gallery</a>
+                        </li>
+                        <ul class="navbad">
+                            <li class="nav-item">
+                                <a class="nav-link" href="about_us.html">About Us</a>
+                            </li>
+                        </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact_us.html">Contact US</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            @if(session('status'))
+                <div class="alert alert-info">
+                    {{session('status')}}
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+
+@yield('content')
+
+<!--footer start-->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <h2>About Us</h2>
+                <p>
+                    Smart Eye is a leading provider of information technology, consulting, and business process services. Our dedicated employees offer strategic insights, technological expertise and industry experience.
+                </p>
+                <p>We focus on technologies that promise to reduce costs, streamline processes and speed time-to-market, Backed by our strong quality processes and rich experience managing global... </p>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <h2>Useful Links</h2>
+                <ul class="list-unstyled link-list">
+                    <li><a ui-sref="about" href="#/about">About us</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="portfolio" href="#/portfolio">Portfolio</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="products" href="#/products">Latest jobs</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="gallery" href="#/gallery">Gallery</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="contact" href="#/contact">Contact us</a><i class="fa fa-angle-right"></i></li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-sm-12 map-img">
+                <h2>Contact Us</h2>
+                <address class="md-margin-bottom-40">
+                    BlueDart <br>
+                    Marthandam (K.K District) <br>
+                    Tamil Nadu, IND <br>
+                    Phone: +91 9159669599 <br>
+                    Email: <a href="mailto:info@anybiz.com" class="">info@bluedart.in</a><br>
+                    Web: <a href="smart-eye.html" class="">www.bluedart.in</a>
+                </address>
+            </div>
+        </div>
+
+        <div class="nav-box row clearfix">
+            <div class="inner col-md-9 clearfix">
+                <ul class="footer-nav clearfix">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Servies</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="donate-link col-md-3"><a href="donate.html" class="btn btn-primary "><span class="btn-title">Donate Now</span></a></div>
+        </div>
+    </div>
+</footer>
+<div class="copy">
+    <div class="container">
+        <a href="#">2020 &copy; All Rights Reserved</a>
+        <span>
+                <a><i class="fab fa-github"></i></a>
+                <a><i class="fab fa-twitter"></i></a>
+                <a><i class="fab fa-facebook-f"></i></a>
+            </span>
+    </div>
+</div>
+<a id="back2Top" title="Back to top" href="#">&#10148;</a>
+<!-- js files -->
+<script src="/js/front2.js"></script>
+@stack('scripts')
+</body>
+
+</html>
+{{--<!DOCTYPE html>
+<html lang="en">
+
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -239,4 +421,4 @@
     @stack('scripts')
 </body>
 
-</html>
+</html>--}}
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,312 @@
 
 @section('content')
     <!--main content start-->
+    <!-- ******************** Slider Starts Here ******************* -->
+    <div class="slider">
+        <!-- Set up your HTML -->
+        <div class="owl-carousel ">
+            <div class="slider-img">
+                <div class="item">
+                    <div class="slider-img"><img src="/images/slider/slider-3.jpg" alt=""></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
+                                <div class="animated bounceInDown slider-captions">
+                                    <h1 class="slider-title">Most Stylish Free Travel Website</h1>
+                                    <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="slider-img"><img src="/images/slider/slider-1.jpg" alt=""></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
+                            <div class="slider-captions ">
+                                <h1 class="slider-title">It's time for better help.</h1>
+                                <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="slider-img"><img src="/images/slider/slider-2.jpg" alt=""></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
+                            <div class="slider-captions ">
+                                <h1 class="slider-title">Most Attractive Travel Template</h1>
+                                <p class="slider-text hidden-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ################# Our News Starts Here#######################--->
+    <section class="our-blog">
+        <div class="container">
+            <div class="row session-title">
+                <h2> Our News </h2>
+                <p>News about our activities</p>
+            </div>
+            <div class="blog-row row">
+                @foreach($recentPosts as $post)
+                    <div class="col-md-4 col-sm-6">
+                        <div class="single-blog">
+                            <figure style="text-align: center">
+                                <a href="{{route('post.show', $post->slug)}}"><img src="{{$post->getImage()}}" width="340" height="220" alt=""></a>
+                            </figure>
+                            <div class="blog-detail">
+                                <small>{{$post->getDate()}}</small>
+                                <h4><a href="{{route('post.show', $post->slug)}}">{{$post->title}}</a></h4>
+                                <p> {!!$post->description!!}</p>
+                                <div class="link">
+                                    <a href="{{route('post.show', $post->slug)}}">Read more </a><i class="fas fa-long-arrow-alt-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    <!-- ################# Our Articles Starts Here#######################--->
+    <section class="our-blog">
+        <div class="container">
+            <div class="row session-title">
+                <h2> Our Articles </h2>
+                <p>We write interesting articles</p>
+            </div>
+            <div class="blog-row row">
+                @foreach($articles as $article)
+                    <div class="col-md-4 col-sm-6">
+                        <div class="single-blog">
+                            <figure style="text-align: center">
+                                <a href="{{route('article.show', $article->slug)}}"><img src="{{$article->getImage()}}" width="340" height="220" alt=""></a>
+                            </figure>
+                            <div class="blog-detail">
+                                <small>{{$article->getDate()}}</small>
+                                <h4><a href="{{route('article.show', $article->slug)}}">{{$article->title}}</a></h4>
+                                <p> {!!$article->description!!}</p>
+                                <div class="link">
+                                    <a href="{{route('article.show', $article->slug)}}">Read more </a><i class="fas fa-long-arrow-alt-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    <!-- ################# Our Videos Starts Here#######################--->
+    <section class="our-blog">
+        <div class="container">
+            <div class="row session-title">
+                <h2> Our Videos </h2>
+                <p>Videos with us or about us</p>
+            </div>
+            <div class="blog-row row">
+                @foreach($videoposts as $videopost)
+                    <div class="col-md-4 col-sm-6">
+                        <div class="single-blog">
+                            <figure style="text-align: center">
+                                <a href="{{route('videos.show', $videopost->slug)}}"><img src="{{$videopost->getImage()}}" width="340" height="220" alt=""></a>
+                            </figure>
+                            <div class="blog-detail">
+                                <small>{{$videopost->getDate()}}</small>
+                                <h4><a href="{{route('videos.show', $videopost->slug)}}">{{$videopost->title}}</a></h4>
+                                <div class="link">
+                                    <a href="{{route('videos.show', $videopost->slug)}}">Read more </a><i class="fas fa-long-arrow-alt-right"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    <!--################### Our Team Starts Here #######################--->
+    <section class="our-team team-11">
+        <div class="container">
+            <div class="session-title row">
+                <h2>Meet our Team</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit velit</p>
+            </div>
+            <div class="row team-row">
+                @foreach($members as $member)
+                    <div class="col-md-3 col-sm-6">
+                        <div class="single-usr">
+                            <img src="{{$member->getImage()}}" width="200" height="200" alt="">
+                            <div class="det-o">
+                                <h4>{{$member->name}}</h4>
+                                <i>{{$member->position}}</i>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+
+    <!--  ************************* About Us Starts Here ************************** -->
+    <div class="about-us container-fluid">
+        <div class="container">
+            <div class="row natur-row no-margin w-100">
+                <div class="text-part col-md-6">
+                    <h2>About Our Charity</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius faucibus
+                        ligula non congue. Suspendisse at pretium massa, sit amet
+                        vulputate nibh. Nam posuere eros dolor. Donec vel arcu sagittis, pretium nisl </p>
+                    <p> Cras faucibus laoreet nibh, sit amet tincidunt leo mollis in. Etiam eu mauris metus.
+                        Nulla facilisi. Etiam vestibulum,
+                        nisi et convallis elementum, leo orci aliquam metus, id posuere massa neque vitae
+                        arcu.</p>
+
+                    <p>Integer vulputate vehicula dolor a eleifend. Duis aliquam condimentum sapien,
+                        eget tempor justo. Aenean porttitor nibh metus, sollicitudin egestas metus posuere et
+                        . Fusce egestas volutpat metus, in sodales sem bibendum porta. Nunc hendrerit nunc sit
+                        amet tellus posuere, at malesuada sem gravida. Integer maximus ultricies augue, at
+                        dapibus elit bibendum consequat. Cras faucibus tellus eleifend, fermentum purus in,
+                        dapibus sapien. Praesent nec ornare risus. Etiam iaculis, ligula vel gravida
+                        vestibulum, urna justo posuere ante,
+                        id pretium massa arcu sed mi. Nunc a sollicitudin sem. Duis tempus </p>
+                </div>
+                <div class="image-part col-md-6">
+                    <div class="about-quick-box row">
+                        <div class="col-md-6">
+                            <div class="about-qcard">
+                                <i class="fas fa-user"></i>
+                                <p>Becom a Volunteer</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="about-qcard ">
+                                <i class="fas fa-search-dollar red"></i>
+                                <p>Quick Fundrais</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="about-qcard ">
+                                <i class="fas fa-donate yell"></i>
+                                <p>Giv Donation</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="about-qcard ">
+                                <i class="fas fa-hands-helping blu"></i>
+                                <p>Help Someone</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ################# Mission Vision Start Here #######################--->
+    <section class="container-fluid mission-vision">
+        <div class="container">
+            <div class="row mission">
+                <div class="col-md-6 mv-det">
+                    <h1>Our Mission</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer neque libero, pulvinar et elementum quis, facilisis eu ante. Mauris non placerat sapien. Pellentesque tempor arcu non odio scelerisque ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius eros consequat auctor gravida. Fusce tristique lacus at urna sollicitudin pulvinar. Suspendisse hendrerit ultrices mauris.</p>
+                </div>
+                <div class="col-md-6 mv-img">
+                    <img src="/images/misin.jpg" alt="">
+                </div>
+            </div>
+            <div class="row vision">
+                <div class="col-md-6 mv-img">
+                    <img src="/images/vision.jpg" alt="">
+                </div>
+                <div class="col-md-6 mv-det">
+                    <h1>Our Vision</h1>
+                    <p>Ut ultricies lacus a rutrum mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed porta dolor quis felis pulvinar dignissim. Etiam nisl ligula, ullamcorper non metus vitae, maximus efficitur mi. Vivamus ut ex ullamcorper, scelerisque lacus nec, commodo dui. Proin massa urna, volutpat vel augue eget, iaculis tristique dui.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ################# Events Start Here#######################--->
+    <section class="events">
+        <div class="container">
+            <div class="session-title row">
+                <h2>Popular Causes</h2>
+                <p>We are a non-profital & Charity raising money for child education</p>
+            </div>
+            <div class="event-ro row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="event-box">
+                        <img src="/images/events/image_08.jpg" alt="">
+                        <h4>Child Education in Africa</h4>
+                        <p class="raises"><span>Raised : $34,425</span> / $500,000 </p>
+                        <p class="desic">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
+                        <button class="btn btn-success btn-sm">Donate Now</button>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="event-box">
+                        <img src="/images/events/image_06.jpg" alt="">
+                        <h4>Child Education in Africa</h4>
+                        <p class="raises"><span>Raised : $34,425</span> / $500,000 </p>
+                        <p class="desic">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
+                        <button class="btn btn-success btn-sm">Donate Now</button>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="event-box">
+                        <img src="/images/events/image_04.jpg" alt="">
+                        <h4>Child Education in Africa</h4>
+                        <p class="raises"><span>Raised : $34,425</span> / $500,000 </p>
+                        <p class="desic">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
+                        <button class="btn btn-success btn-sm">Donate Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ################# Charity Number Starts Here#######################--->
+    <div class="stats-message">
+        <div class="inner-lay">
+            <div class="container">
+                <div class="row session-title">
+                    <h2>Our Achievements in Numbers</h2>
+                    <p>We can talk for a long time about advantages.
+                        But you can read the following facts in order to make sure of all pluses of our organisation:</p>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3 numb">
+                        <h3>12+</h3>
+                        <span>YEARS OF EXPEREANCE</span>
+                    </div>
+                    <div class="col-sm-3 numb">
+                        <h3>1812+</h3>
+                        <span>HAPPY CHILDRENS</span>
+                    </div>
+                    <div class="col-sm-3 numb">
+                        <h3>52+</h3>
+                        <span>EVENTS</span>
+                    </div>
+                    <div class="col-sm-3 numb">
+                        <h3>48+</h3>
+                        <span>FUNT RAISED</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end main content-->
+@endsection
+
+{{--@extends('layout')
+
+@section('content')
+    <!--main content start-->
     <div class="main-content">
         <div class="container">
             <div class="row">
@@ -49,4 +355,4 @@
         </div>
     </div>
     <!-- end main content-->
-@endsection
+@endsection--}}
