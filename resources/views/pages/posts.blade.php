ade83679e72962258babb3e50a352645424bc107
Sviatoslav <sv_dubov@ukr.net>
2020-08-11 16:24:50 +0300

    Edit Posts views

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -5,7 +5,7 @@
     <div class="main-content">
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-8">
                     @if(session('status'))
                         <div class="alert alert-danger">
                             {{session('status')}}
@@ -13,31 +13,31 @@
                     @endif
                     <article class="post">
                         <div class="post-thumb">
-                            <a href="{{route('post.show', $post->slug)}}"><img src="{{$post->getImage()}}" alt=""></a>
+                            <img src="{{$article->getImage()}}" alt="">
                         </div>
                         <div class="post-content">
                             <div class="entry-header text-center">
-                                @if($post->hasCategory())
+                                @if($article->hasCategory())
                                     <h6>
-                                        <a href="{{route('category.show', $post->category->slug)}}"> {{$post->getCategoryTitle()}}</a>
+                                        <a href="{{route('category.show', $article->category->slug)}}"> {{$article->getCategoryTitle()}}</a>
                                     </h6>
                                 @endif
-                                <h1 class="entry-title">{{$post->title}}</h1>
+                                <h1 class="entry-title">{{$article->title}}</h1>
                             </div>
                             <div class="entry-content">
-                                {!!$post->content!!}
+                                {!!$article->content!!}
                             </div>
                             <div class="entry-content">
-                                {!!$post->author!!}
+                                {!!$article->author!!}
                             </div>
                             </br>
                             <div class="tag">
-                                @foreach($post->tags as $tag)
+                                @foreach($article->tags as $tag)
                                     <a href="{{route('tag.show', $tag->slug)}}">{{$tag->title}}</a>
                                 @endforeach
                             </div>
                             <div class="social-share">
-                                <small>{{$post->getDate()}}</small>
+                                <small>{{$article->getDate()}}</small>
                                 <ul class="text-center pull-right">
                                     <li><a class="s-facebook" href="#"><i class="fab fa-facebook-square"></i></a></li>
                                     <li><a class="s-twitter" href="#"><i class="fab fa-twitter-square"></i></a></li>
@@ -50,14 +50,14 @@
                     <div class="row">
                         <!--blog next previous-->
                         <div class="col-md-6">
-                            @if($post->hasPrevious())
+                            @if($article->hasPrevious())
                                 <div class="single-blog-box">
-                                    <a href="{{route('post.show', $post->getPrevious()->slug)}}">
-                                        <img src="{{$post->getPrevious()->getImage()}}" width="340" height="220" alt="">
+                                    <a href="{{route('articles.show', $article->getPrevious()->slug)}}">
+                                        <img src="{{$article->getPrevious()->getImage()}}" alt="">
                                         <div class="overlay">
                                             <div class="promo-text">
-                                                <p><i class="pull-left fa fa-angle-left"></i></p>
-                                                <h5>{{$post->getPrevious()->title}}</h5>
+                                                <p><i class=" pull-left fa fa-angle-left"></i></p>
+                                                <h5>{{$article->getPrevious()->title}}</h5>
                                             </div>
                                         </div>
                                     </a>
@@ -65,14 +65,14 @@
                             @endif
                         </div>
                         <div class="col-md-6">
-                            @if($post->hasNext())
+                            @if($article->hasNext())
                                 <div class="single-blog-box">
-                                    <a href="{{route('post.show', $post->getNext()->slug)}}">
-                                        <img src="{{$post->getNext()->getImage()}}" width="340" height="220" alt="">
+                                    <a href="{{route('articles.show', $article->getNext()->slug)}}">
+                                        <img src="{{$article->getNext()->getImage()}}" alt="">
                                         <div class="overlay">
                                             <div class="promo-text">
-                                                <p><i class="pull-right fa fa-angle-right"></i></p>
-                                                <h5>{{$post->getNext()->title}}</h5>
+                                                <p><i class=" pull-right fa fa-angle-right"></i></p>
+                                                <h5>{{$article->getNext()->title}}</h5>
                                             </div>
                                         </div>
                                     </a>
@@ -84,8 +84,6 @@
                 </div>
             </div>
         </div>
-    </div>
-    </br>
     <!-- end main content-->
 
 @endsection
