024801905c527c2804eaf91bc954fdb61bef408b
Sviatoslav <sv_dubov@ukr.net>
2020-08-11 15:57:49 +0300

    Add front2 folder and edit webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,57 +11,81 @@ const mix = require('laravel-mix');
  |
  */
 
+// mix.styles([
+// 	'resources/assets/admin/bootstrap/css/bootstrap.min.css',
+// 	'resources/assets/admin/font-awesome/4.5.0/css/font-awesome.min.css',
+// 	'resources/assets/admin/ionicons/2.0.1/css/ionicons.min.css',
+// 	'resources/assets/admin/plugins/iCheck/minimal/_all.css',
+// 	'resources/assets/admin/plugins/datepicker/datepicker3.css',
+// 	'resources/assets/admin/plugins/select2/select2.min.css',
+// 	'resources/assets/admin/plugins/datatables/dataTables.bootstrap.css',
+// 	'resources/assets/admin/dist/css/AdminLTE.min.css',
+// 	'resources/assets/admin/dist/css/skins/_all-skins.min.css'
+// ], 'public/css/admin.css');
+
+// mix.scripts([
+// 	'resources/assets/admin/plugins/jQuery/jquery-2.2.3.min.js',
+// 	'resources/assets/admin/bootstrap/js/bootstrap.min.js',
+// 	'resources/assets/admin/plugins/select2/select2.full.min.js',
+// 	'resources/assets/admin/plugins/datepicker/bootstrap-datepicker.js',
+// 	'resources/assets/admin/plugins/datatables/jquery.dataTables.min.js',
+// 	'resources/assets/admin/plugins/datatables/dataTables.bootstrap.min.js',
+// 	'resources/assets/admin/plugins/slimScroll/jquery.slimscroll.min.js',
+// 	'resources/assets/admin/plugins/fastclick/fastclick.js',
+// 	'resources/assets/admin/plugins/iCheck/icheck.min.js',
+// 	'resources/assets/admin/dist/js/app.min.js',
+// 	'resources/assets/admin/dist/js/demo.js',
+// 	'resources/assets/admin/dist/js/scripts.js'
+// ], 'public/js/admin.js');
+
+// mix.copy('resources/assets/admin/bootstrap/fonts', 'public/fonts');
+// mix.copy('resources/assets/admin/dist/fonts', 'public/fonts');
+// mix.copy('resources/assets/admin/dist/img', 'public/img');
+// mix.copy('resources/assets/admin/plugins/iCheck/minimal/blue.png', 'public/css');
+
+// mix.styles([
+// 	'resources/assets/front/css/bootstrap.min.css',
+// 	'resources/assets/front/css/font-awesome.min.css',
+// 	'resources/assets/front/css/animate.min.css',
+// 	'resources/assets/front/css/owl.carousel.css',
+// 	'resources/assets/front/css/owl.theme.css',
+// 	'resources/assets/front/css/owl.transitions.css',
+// 	'resources/assets/front/css/style.css',
+// 	'resources/assets/front/css/responsive.css'
+// ],'public/css/front.css');
+
+// mix.scripts([
+// 	'resources/assets/front/js/jquery-1.11.3.min.js',
+// 	'resources/assets/front/js/bootstrap.min.js',
+// 	'resources/assets/front/js/owl.carousel.min.js',
+// 	'resources/assets/front/js/jquery.stickit.min.js',
+// 	'resources/assets/front/js/menu.js',
+// 	'resources/assets/front/js/scripts.js'
+// ], 'public/js/front.js');
+
+// mix.copy('resources/assets/front/fonts', 'public/fonts');
+// mix.copy('resources/assets/front/images', 'public/images');
+
 mix.styles([
-	'resources/assets/admin/bootstrap/css/bootstrap.min.css',
-	'resources/assets/admin/font-awesome/4.5.0/css/font-awesome.min.css',
-	'resources/assets/admin/ionicons/2.0.1/css/ionicons.min.css',
-	'resources/assets/admin/plugins/iCheck/minimal/_all.css',
-	'resources/assets/admin/plugins/datepicker/datepicker3.css',
-	'resources/assets/admin/plugins/select2/select2.min.css',
-	'resources/assets/admin/plugins/datatables/dataTables.bootstrap.css',
-	'resources/assets/admin/dist/css/AdminLTE.min.css',
-	'resources/assets/admin/dist/css/skins/_all-skins.min.css'
-], 'public/css/admin.css');
+    'resources/assets/front2/css/bootstrap.min.css',
+    'resources/assets/front2/css/all.min.css',
+    'resources/assets/front2/css/animate.min.css',
+    'resources/assets/front2/plugins/slider/css/owl.carousel.min.css',
+    'resources/assets/front2/plugins/slider/css/owl.theme.default.css',
+    'resources/assets/front2/css/style.css',
+],'public/css/front2.css');
 
 mix.scripts([
-	'resources/assets/admin/plugins/jQuery/jquery-2.2.3.min.js',
-	'resources/assets/admin/bootstrap/js/bootstrap.min.js',
-	'resources/assets/admin/plugins/select2/select2.full.min.js',
-	'resources/assets/admin/plugins/datepicker/bootstrap-datepicker.js',
-	'resources/assets/admin/plugins/datatables/jquery.dataTables.min.js',
-	'resources/assets/admin/plugins/datatables/dataTables.bootstrap.min.js',
-	'resources/assets/admin/plugins/slimScroll/jquery.slimscroll.min.js',
-	'resources/assets/admin/plugins/fastclick/fastclick.js',
-	'resources/assets/admin/plugins/iCheck/icheck.min.js',
-	'resources/assets/admin/dist/js/app.min.js',
-	'resources/assets/admin/dist/js/demo.js',
-	'resources/assets/admin/dist/js/scripts.js'
-], 'public/js/admin.js');
+    'resources/assets/front2/js/jquery-3.2.1.min.js',
+    'resources/assets/front2/js/bootstrap.min.js',
+    'resources/assets/front2/js/popper.min.js',
+    'resources/assets/front2/plugins/scroll-fixed/jquery-scrolltofixed-min.js',
+    'resources/assets/front2/plugins/slider/js/owl.carousel.min.js',
+    'resources/assets/front2/js/scripts.js',
+    'resources/assets/front2/js/scroll.js'
+], 'public/js/front2.js');
 
-mix.copy('resources/assets/admin/bootstrap/fonts', 'public/fonts');
-mix.copy('resources/assets/admin/dist/fonts', 'public/fonts');
-mix.copy('resources/assets/admin/dist/img', 'public/img');
-mix.copy('resources/assets/admin/plugins/iCheck/minimal/blue.png', 'public/css');
-
-mix.styles([
-	'resources/assets/front/css/bootstrap.min.css',
-	'resources/assets/front/css/font-awesome.min.css',
-	'resources/assets/front/css/animate.min.css',
-	'resources/assets/front/css/owl.carousel.css',
-	'resources/assets/front/css/owl.theme.css',
-	'resources/assets/front/css/owl.transitions.css',
-	'resources/assets/front/css/style.css',
-	'resources/assets/front/css/responsive.css'
-],'public/css/front.css');
-
-mix.scripts([
-	'resources/assets/front/js/jquery-1.11.3.min.js',
-	'resources/assets/front/js/bootstrap.min.js',
-	'resources/assets/front/js/owl.carousel.min.js',
-	'resources/assets/front/js/jquery.stickit.min.js',
-	'resources/assets/front/js/menu.js',
-	'resources/assets/front/js/scripts.js'
-], 'public/js/front.js');
-
-mix.copy('resources/assets/front/fonts', 'public/fonts');
-mix.copy('resources/assets/front/images', 'public/images');
+mix.copy('resources/assets/front2/fonts', 'public/fonts');
+mix.copy('resources/assets/front2/images', 'public/images');
+mix.copy('resources/assets/front2/webfonts', 'public/webfonts');
+mix.copy('resources/assets/front2/scss', 'public/scss');
