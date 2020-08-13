d9291f6d59e1a68b442ba92c9a3471cea665c878
Sviatoslav <sv_dubov@ukr.net>
2020-08-13 16:56:08 +0300

    Edit footer partial and add it to Gallery show view

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,5 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>NGO Website</title>
+    <link rel="shortcut icon" href="/images/fav.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="/images/fav.jpg">
+    <!-- common css -->
+    <link rel="stylesheet" href="/css/front2.css">
+    @yield('assets')
+</head>
+<!--footer start-->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-12">
+                <h2>About Us</h2>
+                <p>
+                    Smart Eye is a leading provider of information technology, consulting, and business process
+                    services. Our dedicated employees offer strategic insights, technological expertise and industry
+                    experience.
+                </p>
+                <p>We focus on technologies that promise to reduce costs, streamline processes and speed time-to-market,
+                    Backed by our strong quality processes and rich experience managing global... </p>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <h2>Useful Links</h2>
+                <ul class="list-unstyled link-list">
+                    <li><a ui-sref="about" href="#/about">About us</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="portfolio" href="#/portfolio">Portfolio</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="products" href="#/products">Latest jobs</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="gallery" href="#/gallery">Gallery</a><i class="fa fa-angle-right"></i></li>
+                    <li><a ui-sref="contact" href="#/contact">Contact us</a><i class="fa fa-angle-right"></i></li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-sm-12 map-img">
+                <h2>Contact Us</h2>
+                <address class="md-margin-bottom-40">
+                    BlueDart <br>
+                    Marthandam (K.K District) <br>
+                    Tamil Nadu, IND <br>
+                    Phone: +91 9159669599 <br>
+                    Email: <a href="mailto:info@anybiz.com" class="">info@bluedart.in</a><br>
+                    Web: <a href="smart-eye.html" class="">www.bluedart.in</a>
+                </address>
+            </div>
+        </div>
+
+        <div class="nav-box row clearfix">
+            <div class="inner col-md-9 clearfix">
+                <ul class="footer-nav clearfix">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Servies</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="donate-link col-md-3"><a href="donate.html" class="btn btn-primary "><span class="btn-title">Donate Now</span></a>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="copy">
+    <div class="container">
+        <a href="#">2020 &copy; All Rights Reserved</a>
+        <span>
+                <a><i class="fab fa-github"></i></a>
+                <a><i class="fab fa-twitter"></i></a>
+                <a><i class="fab fa-facebook-f"></i></a>
+            </span>
+    </div>
+</div>
+<a id="back2Top" title="Back to top" href="#">&#10148;</a>
+
+{{--@yield('content')--}}
+
+<!-- js files -->
+<script src="/js/front2.js"></script>
+@stack('scripts')
+</body>
+
+</html>
+
+{{--<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -64,7 +151,7 @@
     </div>
 </div>
 
-{{--@yield('content')--}}
+--}}{{--@yield('content')--}}{{--
 
 <!--footer start-->
 <footer class="footer-widget-section">
@@ -187,4 +274,4 @@
 @stack('scripts')
 </body>
 
-</html>
+</html>--}}
