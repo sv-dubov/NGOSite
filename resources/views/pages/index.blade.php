49d8230a5d31feb57effb93cdbad512150208d04
Sviatoslav <sv_dubov@ukr.net>
2020-08-20 16:30:20 +0300

    Add links to index (home) page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -54,7 +54,7 @@
     <section class="our-blog">
         <div class="container">
             <div class="row session-title">
-                <h2> Our News </h2>
+                <h2><a href="/posts"> Our News </a></h2>
                 <p>News about our activities</p>
             </div>
             <div class="blog-row row">
@@ -82,7 +82,7 @@
     <section class="our-blog">
         <div class="container">
             <div class="row session-title">
-                <h2> Our Articles </h2>
+                <h2><a href="/articles"> Our Articles </a></h2>
                 <p>We write interesting articles</p>
             </div>
             <div class="blog-row row">
@@ -110,7 +110,7 @@
     <section class="our-blog">
         <div class="container">
             <div class="row session-title">
-                <h2> Our Videos </h2>
+                <h2><a href="/videos"> Our Videos </a></h2>
                 <p>Videos with us or about us</p>
             </div>
             <div class="blog-row row">
@@ -137,7 +137,7 @@
     <section class="our-team team-11">
         <div class="container">
             <div class="session-title row">
-                <h2>Meet our Team</h2>
+                <h2><a href="/team"> Meet our Team </a></h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla vel nisl a dictum. Donec ut est arcu. Donec hendrerit velit</p>
             </div>
             <div class="row team-row">
@@ -237,7 +237,7 @@
     <section class="events">
         <div class="container">
             <div class="session-title row">
-                <h2>Recent Projects</h2>
+                <h2><a href="/projects"> Recent Projects </a></h2>
                 <p>The last three projects of our team</p>
             </div>
             <div class="event-ro row">
@@ -275,56 +275,3 @@
     </div>
     <!-- end main content-->
 @endsection
-
-{{--@extends('layout')
-
-@section('content')
-    <!--main content start-->
-    <div class="main-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    @foreach($posts as $post)
-                        <article class="post">
-                            <div class="post-thumb">
-                                <a href="{{route('post.show', $post->slug)}}"><img src="{{$post->getImage()}}" alt=""></a>
-                                <a href="{{route('post.show', $post->slug)}}" class="post-thumb-overlay text-center">
-                                    <div class="text-center">View Post</div>
-                                </a>
-                            </div>
-                            <div class="post-content">
-                                <header class="entry-header text-center">
-                                    @if($post->hasCategory())
-                                        <h6>
-                                            <a href="{{route('category.show', $post->category->slug)}}"> {{$post->getCategoryTitle()}}</a>
-                                        </h6>
-                                    @endif
-                                    <h1 class="entry-title"><a href="{{route('post.show', $post->slug)}}">{{$post->title}}</a></h1>
-                                </header>
-                                <div class="entry-content">
-                                    {!!$post->description!!}
-                                    <div class="btn-continue-reading text-center">
-                                        <a href="{{route('post.show', $post->slug)}}" class="more-link">Continue Reading</a>
-                                    </div>
-                                </div>
-                                <div class="social-share">
-                                    <span class="social-share-title pull-left text-capitalize">{{$post->getDate()}}</span>
-                                    <ul class="text-center pull-right">
-                                        <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                                        <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                                        <li><a class="s-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
-                                        <li><a class="s-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                                        <li><a class="s-instagram" href="#"><i class="fa fa-instagram"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </article>
-                    @endforeach
-                    {{$posts->links()}}
-                </div>
-                @include('pages._sidebar')
-            </div>
-        </div>
-    </div>
-    <!-- end main content-->
-@endsection--}}
